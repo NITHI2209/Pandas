f21ecdf7c09abdcf00a67ae38d684a767234ad76
NITHI2209 <nithyasree065@gmail.com>
2026-01-17 12:42:12 +0530

    Create ModifyingSeries(8).py

---
+++ ModifyingSeries(8).py
@@ -0,0 +1,14 @@
+Modifying series
+>>> s2["Mango"] = 2.8
+>>> print(s2)
+Avocado         2.0
+Guava           2.6
+Blackberries    2.0
+Oranges         0.9
+Banana          1.1
+Apples          0.3
+Kiwi            1.1
+Pomegranate     1.7
+Mango           2.8
+Cherries        1.0
+Name: Protein, dtype: float64
