c1b5a0208fa2ed26e207c307f76aeb1a5c9b4c24
NITHI2209 <nithyasree065@gmail.com>
2026-01-17 11:25:04 +0530

    Create Theoritical(1).py

---
+++ Theoritical(1).py
@@ -0,0 +1,19 @@
+Pandas Definition:
+Pandas is a powerful open-source Python library used for data analysis and data manipulation.
+It provides easy-to-use tools to work with structured data, especially tables (rows and columns).
+
+Key points:
+1)Built on top of Python and NumPy
+2)Used to handle Excel-like or SQL-like data
+3)Allows reading data from CSV, Excel, SQL, JSON, etc.
+4)Provides two main data structures:
+         1) Series → 1-dimensional data (like a column)
+         2) DataFrame → 2-dimensional data (like a table)  
+
+why pandas:
+1)Import datasets
+2)Data cleaning
+3)Size mutability
+4)Reshapning and pivoting of datasets
+5)Efficient Manipulation and extraction
+6) Stastistical analysis
