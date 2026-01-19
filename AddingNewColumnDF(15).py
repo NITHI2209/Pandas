f3e1820e13134ffd04d5abf87a3fcfb1b7e8a69c
NITHI2209 <nithyasree065@gmail.com>
2026-01-19 18:34:48 +0530

    Create AddingNewColumnDF(15).py

---
+++ AddingNewColumnDF(15).py
@@ -0,0 +1,9 @@
+>>> df["Promoted Salary"] = df["Salary"] * 10
+>>> print(df)
+      Name   Age     Dept   Salary  Promoted Salary
+0    Alice  25.0       HR  50000.0         500000.0
+1      Bob  30.0       IT  60000.0         600000.0
+2  Charlie  35.0  Finance  70000.0         700000.0
+3    David   NaN       IT  62000.0         620000.0
+4      Eve  29.0       HR      NaN              NaN
+5    Alice  25.0       HR  50000.0         500000.0
